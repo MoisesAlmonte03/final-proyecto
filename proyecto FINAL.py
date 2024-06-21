3301c425a637576af4d9e53f59ae92a78e045ff1
MoisesAlmonte03 <moisesalmonter@gmail.com>
2024-06-21 18:35:32 -0400

    Update proyecto FINAL.py
    añadido los import para json y limpiar consola. creacion de clase reserva con cada atributo que tendra la reserva y con un id que vaya aumentando

--- proyecto FINAL.py
+++ proyecto FINAL.py
@@ -1,1 +1,14 @@
 #proyecto final
+import json
+import os
+
+class Reserva:
+    ReservaID = 1
+
+    def __init__(self, nombre_cliente, numero_personas, fecha, hora):
+        self.id = Reserva.ReservaID
+        self.nombre_cliente = nombre_cliente
+        self.numero_personas = numero_personas
+        self.fecha = fecha
+        self.hora = hora
+        Reserva.ReservaID += 1
